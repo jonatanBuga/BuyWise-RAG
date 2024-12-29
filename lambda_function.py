4627e5ed314f92105b784c664eb14758ac4704bd
jonatanbuga <yhonabo@ac.sce.ac.il>
2024-12-29 18:17:21 +0200

    update lambda function and push to aws the changes

--- lambda_function.py
+++ lambda_function.py
@@ -11,8 +11,38 @@ load_dotenv()
 
 os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
 
+def generate_prompt(query: str) -> str:
+    """
+    Generate a prompt for the LLM using the user's query and retrieved context.
+
+    Args:
+        query (str): The original user query.
+        retrieved_context (List[str]): List of retrieved items from the RAG retriever.
+
+    Returns:
+        str: A detailed prompt to be sent to the LLM.
+    """
+
+    
+    prompt = (
+        "You are a shopping assistant that helps users create detailed shopping lists based on their needs.\n"
+        f"The user provided the following request:\n"
+        f"{query}\n\n"
+        "Please follow this format for your response:\n"
+        "1. Shopping List: Return **only** the shopping list in this format:\n"
+        "   - Item name (e.g., bananas): Quantity (e.g., 7 units).\n"
+        "   - For liquid items, specify the quantity in milliliters.\n"
+        "   - For items that require grams, specify the quantity in grams.\n"
+        "   - After each item, append the letters 'tj' (e.g., Bananas: 7 units tj).\n\n"
+        "2. Details about the list: After the shopping list, provide a summary of the items, such as:\n"
+        "   - Total number of items.\n"
+        "   - Categories of items (e.g., fruits, vegetables, dairy, etc.).\n"
+        "   - Any additional notes or considerations based on the user's request.\n"
+    )
+    return prompt
 
 def get_response(prompt):
+    complite_prompt = generate_prompt(prompt)
     client = OpenAI(
             api_key=os.environ.get("OPENAI_API_KEY"), 
         )
@@ -20,7 +50,7 @@ def get_response(prompt):
     response = client.chat.completions.create(
         messages=[{"role": "system", "content": "You are a helpful assistant."},{
             "role": "user",
-            "content": prompt,
+            "content": complite_prompt,
         }],
         model="gpt-4o-mini",
         max_tokens=700
